12f0befbfca8e3789482c922b115b0637e5f0a02
djKooks <inylove82@gmail.com>
2022-03-20 10:27:59 +0900

    refactoring

--- src/main/scala/com/kination/util/YamlParser.scala
+++ src/main/scala/com/kination/util/YamlParser.scala
@@ -6,10 +6,12 @@ import io.circe._
 import io.circe.generic.auto._
 import io.circe.yaml
 
+import scala.io.Source.fromFile
+
 
 object YamlParser {
   def sourceFromFile(configFilePath: String): DataSource = {
-    val lines = scala.io.Source.fromFile(configFilePath).mkString.stripMargin
+    val lines = fromFile(configFilePath).mkString.stripMargin
     val json: Either[ParsingFailure, Json] = yaml.parser.parse(lines)
     json.leftMap(err => err).flatMap(_.as[DataSource]).valueOr(throw _)
   }
